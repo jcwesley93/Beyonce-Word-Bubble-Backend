654bab3ad1385367da93b826af33380d395dbef6
Jaela Wesley <github email address>
2019-04-15 14:55:23 -0400

    added scrapped data for other albums

--- data/data.rb
+++ data/data.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-doc = Nokogiri::HTML(open('https://www.beyonce.com/album/lemonade-visual-album/lyrics/'))
+doc = Nokogiri::HTML(open('https://www.beyonce.com/album/dangerously-in-love/lyrics/'))
 
 doc.css('p').each do |paragraph|
   p paragraph.to_s
