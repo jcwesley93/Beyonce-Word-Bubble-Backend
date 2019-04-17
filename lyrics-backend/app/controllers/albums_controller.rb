834a174c8e007c073a33db3f274d603d8210fb83
char <25884226+char@users.noreply.github.com>
2019-04-17 01:25:41 -0400

    Bring the beat in

--- lyrics-backend/app/controllers/albums_controller.rb
+++ lyrics-backend/app/controllers/albums_controller.rb
@@ -3,8 +3,9 @@ class AlbumsController < ApplicationController
 		@albums = Album.all
 		render json: @albums
 	end
+
 	def show
 		@album = Album.find_by(id: params[:id])
-		render json: @album.songs
+		render json: @album.to_json(methods: [:histogram])
 	end
 end
--- lyrics-backend/app/models/album.rb
+++ lyrics-backend/app/models/album.rb
@@ -1,3 +1,11 @@
 class Album < ApplicationRecord
 	has_many :songs
+
+	def histogram
+		histo = Hash.new
+		songs.each do |song|
+			histo = histo.merge(song.histogram)
+		end
+		histo
+	end
 end
